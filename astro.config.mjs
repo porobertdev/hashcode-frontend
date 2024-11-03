72847324e5bfaf30490b990854ec39781ae05041
Robert P <porobertdev@gmail.com>
2024-11-03 18:27:31 +0200

    Install Cloudflare adapter for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
-
-import node from "@astrojs/node";
+import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "hybrid",
-  adapter: node({
-    mode: "standalone"
-  })
+    integrations: [tailwind()],
+    output: 'hybrid',
+    adapter: cloudflare(),
 });
