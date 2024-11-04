48636dfec1c24b4eddfa5fa1258ac0adb7dcb6de
Robert P <porobertdev@gmail.com>
2024-11-04 18:07:14 +0200

    Set-up SEO meta tags using `astro-seo`
    * Reference: https://egghead.io/lessons/astro-implement-seo-in-an-astro-website-with-astro-seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import cloudflare from '@astrojs/cloudflare';
+import metaTags from 'astro-meta-tags';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
+    integrations: [tailwind(), metaTags()],
     output: 'hybrid',
     adapter: cloudflare(),
 });
